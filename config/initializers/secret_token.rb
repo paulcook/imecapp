5ca4666f82ecfde465eead8f280ed9db56f26c20
Paul Cook <paul.cook@alternateversion.com>
2012-03-21 23:10:27 -0400

    initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Imecapp::Application.config.secret_token = '29536b31861139e59a43e9c947c545a8d610f2c8eca6ea29093b4658145920361d0124e945cf0cfcc82826c04c137aa472d3d8e8fd0fc920c8dcb63ed8ac3090'
