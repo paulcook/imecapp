fce5a90869cb1ed989759b315094d9022f2f6f88
Paul Cook <paul.cook@alternateversion.com>
2012-04-19 09:30:33 -0400

    adding registration for providers

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,2 +1,17 @@
 class ProvidersController < ApplicationController
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.new(params[:company])
+
+    if @company.save
+      flash[:success] = "You have successfully added a company."
+      redirect_to new_provider_user_path(@company)
+    else
+      render :new
+    end
+  end
+
 end
